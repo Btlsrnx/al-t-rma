63bf7ed4c761cf0bb8358f16543c41b79d61a7af
Btlsrnx <113302522+Btlsrnx@users.noreply.github.com>
2022-11-27 01:07:50 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Fighter f1 = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter f2 = new Fighter("Alex" ,15,100,90,0);
+        Ring r = new Ring(f1,f2 ,90 ,100);
+        r.run();
+    }
+}
