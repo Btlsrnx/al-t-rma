f9d6afa5a1e7e2eb6f8979dd03af4ec2f95be899
Btlsrnx <113302522+Btlsrnx@users.noreply.github.com>
2022-12-02 01:08:08 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,56 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[][] matris= {{5,3,3},{6,5,2},{}};
-        int[][] transpoz=new int[3][2];
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        Scanner input = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin=false;
+        boolean isWrong=false;
 
-
-        for (int i=0;i< 3;i++){
-            for (int j=0;j<2;j++){
-                transpoz[i][j]=matris[j][i];
-                System.out.print(" "+transpoz[i][j]);
+        while (right < 5) {
+            System.out.println("Lütfen bir sayı giriniz:");
+            selected = input.nextInt();
+            if (selected < 0 || selected > 101) {
+                System.out.println("Lütfen 0 ve 100 aralığında bir değer giriniz:");
+                if (!isWrong){
+                    isWrong=true;
+                    System.out.println("Hatalı girdiğiniz için hakkınızdan düşülecektir.");
+                }else{
+                    System.out.println("Çok fazla hatalı giriş yaptınız! Kalan hakkınız:"+(5-(++right)));
+                }
+                continue;
             }
-            System.out.println();
+            if (selected == number) {
+                System.out.println("Tebrikler tahmin ettiğiniz sayı doğru !Tahmin ettiğiniz sayı :" + number);
+                isWin=true;
+                break;
+            } else {
+                wrong[right] = selected;
+                right++;
+                System.out.println("Hatalı bir sayı girdiniz! Lütfen tekrar deneyiniz:");
+            }
+            if (selected > number) {
+                System.out.println(selected + " sayısı,gizli sayıdan büyüktür!");
+            } else {
+                System.out.println(selected + " sayısı,gizli sayıdan küçüktür!");
+            }
+            System.out.println("Kalan hakkı:" + (5 - right));
         }
 
-    }
+         if (!isWin){
+        System.out.println("Kaybedenler kulübüne hoşgeldin ciğerim!");
+     }
+        System.out.println("GİZLİ SAYI:"+number);
+        System.out.println("Tahminleriniz:"+ Arrays.toString(wrong));
+   }
+
 }
+
+
+
